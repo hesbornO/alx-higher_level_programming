2ff93f0f3ae3ee97d6ae4aba6e91f784875ff403
hesbornO <46963355+hesbornO@users.noreply.github.com>
2023-01-10 21:44:25 +0300

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" lookup method """
+
+
+def lookup(obj):
+    """ lookup definition
+    Returns:
+        the list of available attributes and methods of an object
+    """
+    return dir(obj)
