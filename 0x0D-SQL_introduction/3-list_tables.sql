ab28a1affbce51150ed7409aabaa8417850944fa
hesbornO <46963355+hesbornO@users.noreply.github.com>
2023-02-15 01:42:35 +0300

    Create 3-list_tables.sql

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Replace YOUR_USERNAME and YOUR_PASSWORD with your MySQL credentials
+USERNAME="YOUR_USERNAME"
+PASSWORD="YOUR_PASSWORD"
+
+# Check if the database name was passed as an argument
+if [ $# -eq 0 ]; then
+  echo "Please provide the name of the database as an argument"
+  exit 1
+fi
+
+DB_NAME=$1
+
+# Use mysql to connect to the MySQL server and list the tables in the given database
+mysql -u $USERNAME -p$PASSWORD -e "USE $DB_NAME; SHOW TABLES;"
