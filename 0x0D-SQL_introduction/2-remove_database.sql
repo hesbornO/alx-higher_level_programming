290e658e465f89f5e9f9c77a46f1c96e0abae4cd
hesbornO <46963355+hesbornO@users.noreply.github.com>
2023-02-15 01:51:48 +0300

    Update 2-remove_database.sql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Replace YOUR_USERNAME and YOUR_PASSWORD with your MySQL credentials
-USERNAME="YOUR_USERNAME"
-PASSWORD="YOUR_PASSWORD"
+
 DB_NAME="hbtn_0c_0"
 
 # Use mysqlshow to check if the database exists
