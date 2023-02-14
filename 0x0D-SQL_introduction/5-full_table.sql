b0d68822e8e4e6fb44384a25e2e0930e186a2865
hesbornO <46963355+hesbornO@users.noreply.github.com>
2023-02-15 01:46:36 +0300

    Create 5-full_table.sql

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Replace YOUR_USERNAME and YOUR_PASSWORD with your MySQL credentials
+USERNAME="YOUR_USERNAME"
+PASSWORD="YOUR_PASSWORD"
+
+# Check if the database name was passed as an argument
+if [ $# -eq 0 ]; then
+  echo "Please provide the name of the database as an argument"
+  exit 1
+fi
+
+DB_NAME=$1
+
+# Use mysql to connect to the MySQL server and print the table description
+mysql -u $USERNAME -p$PASSWORD -e "USE $DB_NAME; SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE FROM information_schema.COLUMNS WHERE TABLE_NAME = 'first_table';"
